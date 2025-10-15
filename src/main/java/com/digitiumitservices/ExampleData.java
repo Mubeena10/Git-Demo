99b873acc69db305565d3fcd2f75efa1edbc5477
Mubeena <mbnmuna10@gmail.com>
2025-10-15 12:46:41 +0530

    Second sentence added

--- src/main/java/com/digitiumitservices/ExampleData.java
+++ src/main/java/com/digitiumitservices/ExampleData.java
@@ -4,6 +4,7 @@ public class ExampleData {
 	
 	public void sample() {
 		System.out.println("Hii This is Mubeena....");
+		System.out.println("This is created for second commit for creating the new branch....");
 	}
 
 }
